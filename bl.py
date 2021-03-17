d65434dc77c86d2dcbfc5d5471ce5d0aa3a5bdef
Rishabh Tatiraju <me@rishabh.blog>
2021-03-17 18:13:02 +0530

    Added case insensitive sorting

--- bl.py
+++ bl.py
@@ -127,8 +127,8 @@ def get_artists():
         if name not in artist_names:
             artist_names.append(name)
 
-    artist_names.sort()
-    
+    artist_names.sort(key=lambda x: x.lower())
+
     result = []
     for name in artist_names:
         result.append({
