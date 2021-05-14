8b3d5ee840c6f3a003a98156e2385af062e9842c
Rishabh Tatiraju <me@rishabh.blog>
2021-05-14 19:06:23 +0000

    Changed start script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ git pull
 pip3 install -r requirements.txt
 sudo kill -9 $(sudo lsof -t -i:7474)
 python3 migration.py
-nohup python3 main.py &
+pm2 start main.py --name hope-server --interpreter python3
